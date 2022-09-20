451e684eb1f534807571ed851ec2ec4c8930c813
Luigi <luigi.ciuffi@hotmail.com>
2022-09-19 23:42:44 -0300

    altura 0 em telas pequenas

--- src/components/Login/Container.jsx
+++ src/components/Login/Container.jsx
@@ -1,6 +1,6 @@
 export const Container = (props) => {
     return (
-        <div className="flex w-full justify-center gap-8 mt-[10vh]">
+        <div className="flex w-full justify-center gap-8 md:mt-[10vh] sm:mt-[0vh]">
             {props.children}
         </div>
     );
